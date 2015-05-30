975e219384d83e891f7502d278d6bc0865ddf58b
monica <monica@monica-PC.fritz.box>
2015-05-30 20:30:53 +0200

    todos view coded as in example

--- public_html/js/views/todos.js
+++ public_html/js/views/todos.js
@@ -1,7 +1,60 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+var app = app || {};
 
+// Todo Item view
+// --------------
+
+// The DOM element for a todo item
+app.TodoView = Backbone.View.extend({
+  
+  // .. is a list tag
+  tagname: 'li',
+  
+  // Cache the template function for a single item
+  template: _.template( $('#item-template').html() ),
+  
+  // The DOM events specific to an item
+  events: {
+    'dbclick label': 'edit',
+    'keypress .edit': 'updateOnEnter',
+    'blur .edit': 'close'
+  },
+  
+  // The TodoView listens for changes to its model, re-rendering. Since 
+  // there's a one-to-one correspondance between a **Todo** and a **Todo View**
+  // in this app, we set a direct reference on hte model for convenience
+  initialize: function() {
+    this.listenTo(this.model, 'change', 'this.render');
+  },
+  
+  // Re-renders the titles of the todo Item
+  render: function() {
+    this.$el.html( this.template( this.model.attributes ) );
+    this.$input = this.$('.edit');
+    return this;
+  },
+  
+  // Switch this view into 'editing' mode, displaying the input field
+  edit: function() {
+    this.$el.addClass('editing');
+    this.$input.focus();
+  },
+  
+  // Close the 'editing' mode, saving changes to the Todo
+  close: function() {
+    var value = this.$input.val().trim();
+    
+    if (value) {
+      this.model.save( {title: value} );
+    }
+    
+    this.$el.removeClass('editing');
+  },
+  
+  // If you hit 'enter', we're through editing the item
+  updateOnEnter: function( e ) {
+    if ( e.which === ENTER_KEY ) {
+      this.close();
+    }
+  }
+});
